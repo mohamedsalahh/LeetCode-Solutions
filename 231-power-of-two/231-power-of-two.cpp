f593a4e4be03834009e88b857d315694c6208973
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-07-19 04:04:18 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (69.98%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i = 1; i <= 31; i++)
+        for(int i = 0; i < 31; i++)
         {
-            if((n^(1<<(i-1))) == 0) return true;
+            if((n^(1<<i)) == 0) return true;
         }
         return false;
     }
