70fda0b2f50f56073c147cf6554bac861d0366fd
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-07-20 14:31:17 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (74.81%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int ans = 0;
+        while(z)
+        {
+            ans++;
+            z &= z-1;
+        }
+        return ans;
+    }
+};
