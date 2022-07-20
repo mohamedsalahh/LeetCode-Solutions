2cd56e2da559fede493c8998dfad6967cc2d7dec
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-07-20 22:43:00 +0200

    Time: 207 ms (100.00%), Space: 67.8 MB (100.00%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -13,17 +13,16 @@ public:
     
     int maximumSum(vector<int>& nums) {
         int ans = -1;
-        vector<vector<int>> v(90, vector<int>(2, -1));
+        vector<vector<int>> v(90);
         for(int i = 0; i < nums.size(); ++i)
         {
-            if(nums[i] > v[digitsSum(nums[i])][0])
-                v[digitsSum(nums[i])][0] = nums[i];
-            sort(v[digitsSum(nums[i])].begin(), v[digitsSum(nums[i])].end());
+            v[digitsSum(nums[i])].push_back(nums[i]);
         }
         for(int i = 0; i < 90; ++i)
         {
-            if(v[i][0] == -1 || v[i][1] == -1)
+            if(v[i].size() <= 1)
                 continue;
+            sort(v[i].begin(), v[i].end(), greater<int>());
             ans = max(ans, v[i][0] + v[i][1]);
         }
         return ans;
