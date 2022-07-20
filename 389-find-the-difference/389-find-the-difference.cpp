7a81770623649d48b2fa9edf61a207b7a9d11b0f
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-07-20 21:44:57 +0200

    Time: 3 ms (77.94%), Space: 6.7 MB (25.69%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> freq(26, 0);
+        int ans = 0;
         for(int i = 0; i < s.size(); ++i)
         {
-            freq[s[i]-'a']++;
+            ans ^= s[i] ^ t[i];
         }
-        for(int i = 0; i < t.size(); ++i)
-        {
-            if(freq[t[i]-'a'])
-                freq[t[i]-'a']--;
-            else return t[i];
-        }
-        return ' ';
+        return ans ^ t.back();
     }
 };
