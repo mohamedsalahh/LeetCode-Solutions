f0ca2034765b54e3f96629e6532914356036ba77
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-07-31 07:06:51 +0200

    Time: 614 ms (5.10%), Space: 92.9 MB (68.48%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -4,12 +4,12 @@ public:
         vector<int> bitCount(32, 0), ans(nums.size(), 0);;
         for(int num: nums)
         {
-            for(int i = 0; i < 32; ++i)
+            for(int i = 0; i < maximumBit; ++i)
                 bitCount[i] += (num >> i) & 1;
         }
         
         int idx = nums.size()-1;
-        for(int i = 0; i < nums.size() && idx >= 0; ++i, --idx)
+        for(int i = 0; i < nums.size(); ++i, --idx)
         {
             int k = 0;
             for(int i = 0; i < maximumBit; ++i)
