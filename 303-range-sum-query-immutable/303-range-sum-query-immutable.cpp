c041681288a99ccc907fd876e8e6fedeed42d3a2
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-02 05:29:08 +0200

    Time: 62 ms (24.53%), Space: 17 MB (74.01%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -5,9 +5,7 @@ public:
     NumArray(vector<int>& nums) {
         prefixSum = vector<int>(nums.size()+1, 0);
         for(int i = 1; i <= nums.size(); ++i)
-        {
             prefixSum[i] = prefixSum[i-1] + nums[i-1]; 
-        }
     }
     
     int sumRange(int left, int right) {
