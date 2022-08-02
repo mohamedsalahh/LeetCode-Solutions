da9560d415f07d50922b5d3709c9e55e6ea3a995
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-02 05:56:01 +0200

    Time: 32 ms (56.44%), Space: 6.2 MB (7.27%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        cout << log(n) / log(3) << " " << ceil(log(n) / log(3));
         if(n == 0)
             return false;
-        return ceil(log(n) / log(3)) - log(n) / log(3) <= 0.000000000009;
+        return fmod(log10(n) / log10(3), 1) == 0;
     }
 };
