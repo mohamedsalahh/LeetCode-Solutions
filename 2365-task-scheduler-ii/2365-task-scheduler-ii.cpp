82ff98bda85543457a903d01cb1698971d858aab
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-07 02:02:46 +0200

    Time: 436 ms (100.00%), Space: 100.1 MB (100.00%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     long long taskSchedulerII(vector<int>& tasks, int space) {
         map<int, long long> mp;
-        long long ans = 1;
-        mp[tasks[0]] = 1;
-        for(int i = 1; i < tasks.size(); ++i)
+        long long ans = 0;
+        for(int i = 0; i < tasks.size(); ++i)
         {
             ans++;
             if(mp.count(tasks[i]) != 0 && ans - mp[tasks[i]]  <= space)
