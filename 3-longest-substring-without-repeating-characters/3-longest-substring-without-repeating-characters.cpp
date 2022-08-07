107013cc6b0394a079a6c127056d1b0b774f81dd
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-07 23:48:59 +0200

    Time: 106 ms (10.97%), Space: 8.3 MB (74.62%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> mp;
+        vector<int> mp(226, -1);
         int ans = 0;
+        int cnt = 0;
         for(int i = 0; i < s.size(); ++i)
         {
-            int cnt = 0;
-            for(int j = i; j < s.size(); ++j)
+            if(mp[s[i]] != -1)
             {
-                if(mp[s[j]])
-                    break;
-                cnt++;
-                mp[s[j]] = 1;
+                ans = max(ans, cnt);
+                cnt = i - mp[s[i]];
+                for(int k = 0; k < 226; ++k)
+                {
+                    if(mp[k] < mp[s[i]])
+                        mp[k] = -1;
+                }
+                mp[s[i]] = i;
+                continue;
             }
-            mp.clear();
-            ans = max(ans, cnt);   
+            cnt++;
+            mp[s[i]] = i;
         }
+        ans = max(ans, cnt);
         return ans;
     }
 };
