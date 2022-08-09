40c1b2efb5c0179f4539ebbe30f6853a53ab5fef
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-09 10:34:19 +0200

    Time: 4 ms (24.75%), Space: 5.9 MB (64.41%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(long long i = 1; i < 100000; ++i)
+        for(long long i = 1; i < 50000; ++i)
             if(i * i == num)
                 return true;
         return false;
