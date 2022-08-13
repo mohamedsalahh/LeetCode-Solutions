b1113c50325cde94fecf1f3a0dd5b5ed014223c2
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-13 13:00:47 +0200

    Time: 16 ms (59.84%), Space: 10.8 MB (39.74%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> rows(numRows, "");
+        
+        for(int i = 0; i < s.size();)
+        {
+            for(int j = 0; j < numRows && i < s.size(); ++j)
+                rows[j].push_back(s[i++]);
+            
+            for(int j = numRows-2; j >= 1 && i < s.size(); --j)
+                rows[j].push_back(s[i++]);
+        }
+        string ans = "";
+        for(int i = 0; i < numRows; ++i)
+            ans += rows[i];
+        return ans;
+    }
+};
