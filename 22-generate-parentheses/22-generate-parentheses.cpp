07da762f49a67f4a7abeeb7fa61ba6d58907dc16
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-13 18:53:35 +0200

    Time: 9 ms (36.10%), Space: 18.4 MB (9.22%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     void solve(string s, int openBrackets, int n, vector<string>& ans)
     {
-        if(openBrackets < 0)
-            return;
         if(n == 0)
         {
             if(openBrackets == 0)
                 ans.push_back(s);
             return;
         }
+        
+        if(openBrackets < 0)
+            return;
         solve(s+"(", openBrackets+1, n-1, ans);
         solve(s+")", openBrackets-1, n-1, ans);
     }
