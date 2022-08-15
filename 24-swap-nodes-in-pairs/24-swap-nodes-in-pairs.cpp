8041b2e2d1add42ab54b8c45c432d2d444cb0225
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-15 11:14:49 +0200

    Time: 5 ms (37.74%), Space: 7.6 MB (54.56%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head)
+            return head;
+        if(!head->next)
+            return head;
+        ListNode* temp1 = head, *temp2 = head->next;
+        ListNode* prev = head;
+        while(1)
+        {
+            if(head == temp1)
+                head = temp2;
+            temp1->next = temp2->next;
+            temp2->next = temp1;
+            
+            temp1 = temp1->next;
+            if(!temp1)
+                break;
+            temp2 = temp1->next;
+            if(!temp2)
+                break;
+            
+            prev->next = temp2;
+            prev = temp1;
+        }
+        return head;
+    }
+};
