c4e0360f98665f7d71a4bc37fbdbba0a080fa360
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-17 20:26:30 +0200

    Time: 4 ms (44.15%), Space: 6.2 MB (35.10%) - LeetHub

---
+++ 492-construct-the-rectangle/492-construct-the-rectangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int diff = INT_MAX;
+        vector<int> ans(2);
+        for(int i = 1; i * i <= area; ++i)
+        {
+            if(area % i == 0)
+            {
+                if(abs(i - (area/i)) < INT_MAX)
+                {
+                    diff = abs(i - (area/i));
+                    ans = {i, area/i};
+                }
+            }
+        }
+        if(ans[0] < ans[1]) 
+            swap(ans[0], ans[1]);
+        return ans;
+    }
+};
