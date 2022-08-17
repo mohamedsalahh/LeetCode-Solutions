f57a427e8752256d3637b01f7d0e6ae128c30632
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-17 18:25:29 +0200

    Time: 34 ms (60.20%), Space: 18.9 MB (55.84%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i = 0; i < 9; i += 3)
-        {
-            for(int j = 0; j < 9; j += 3)
-            {
-                vector<int> freq(10, 0);
-                for(int ii = i; ii < i+3; ++ii)
-                {
-                    for(int jj = j; jj < j+3; ++jj)
-                    {
-                        if(board[ii][jj] != '.') {
-                            if(++freq[board[ii][jj] - '0'] > 1)
-                                return false;
-                        }
-                    }
-                }
-            }
-        }
         for(int i = 0; i < 9; ++i)
         {
-            vector<int> freq1(10, 0), freq2(10, 0);
+            vector<int> freq1(10, 0), freq2(10, 0), freq3(10, 0);
             for(int j = 0; j < 9; ++j)
             {
                 if(board[i][j] != '.') {
@@ -29,6 +12,10 @@ public:
                 }
                 if(board[j][i] != '.') {
                     if(++freq2[board[j][i] - '0'] > 1)
+                        return false;
+                }
+                if(board[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3] != '.') {
+                    if(++freq3[board[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3] - '0'] > 1)
                         return false;
                 }
             }
