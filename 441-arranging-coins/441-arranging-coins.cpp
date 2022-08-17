6116fe6917c7d050d165bc9e5b5070bc268e0f12
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-17 19:03:46 +0200

    Time: 2 ms (84.31%), Space: 6 MB (25.11%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        double x = (double) -1 + sqrt(1+4*2*(double)n);
+        double x = sqrt(1+4*2*(double)n) - 1;
         return x / 2;
     }
 };
