38404e547cfa961fd5edc135b333194ff602133a
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-24 22:39:22 +0200

    Time: 4 ms (91.00%), Space: 8.9 MB (57.86%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> idx(1e4+1);
-        vector<int> ans(nums1.size(), -1);
-        for(int i = 0; i < nums2.size(); ++i)
-            idx[nums2[i]] = i;
-        for(int i = 0 ; i < nums1.size(); ++i)
+        map<int, int> mp;
+        vector<int> ans(nums1.size());
+        stack<int> st;
+        for(int i = 0 ; i < nums2.size(); ++i)
         {
-            for(int j = idx[nums1[i]]; j < nums2.size(); ++j)
+            while(!st.empty() && nums2[i] > st.top())
             {
-                if(nums2[j] > nums1[i])
-                {
-                    ans[i] = nums2[j];
-                    break;
-                }
-                
+                mp[st.top()] = nums2[i];
+                st.pop();
             }
+            st.push(nums2[i]);
         }
+        while(!st.empty())
+        {
+            mp[st.top()] = -1;
+            st.pop();
+        }
+        for(int i = 0; i < nums1.size(); ++i)
+            ans[i] = mp[nums1[i]];
         return ans;
     }
 };
