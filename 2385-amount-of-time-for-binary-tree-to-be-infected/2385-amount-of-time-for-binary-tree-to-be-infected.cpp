1d70426712ee1e81b5355b6be086927797ef3d53
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-08-27 23:53:47 +0200

    Time: 1238 ms (11.28%), Space: 202.8 MB (30.99%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* startNode = NULL;
+    map<TreeNode*, TreeNode*> par;
+    void dfs(TreeNode* root, TreeNode* parent, int start)
+    {
+        if(root == NULL)
+            return;
+        if(root->val == start)
+            startNode = root;
+        par[root] = parent;
+        dfs(root->left, root, start);
+        dfs(root->right, root, start);
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        dfs(root, NULL, start);
+        map<TreeNode*, int> vis;
+        int ans = 0;
+        queue<TreeNode*> q;
+        q.push(startNode);
+        vis[startNode] = 1;
+        while(!q.empty())
+        {
+            ans++;
+            int sz = q.size();
+            while(sz--)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left != NULL && !vis[node->left])
+                    q.push(node->left), vis[node->left] = 1;
+                if(node->right != NULL && !vis[node->right])
+                    q.push(node->right), vis[node->right] = 1;
+                if(par[node] != NULL && !vis[par[node]])
+                    q.push(par[node]), vis[par[node]] = 1;
+                
+            }
+        }
+        return ans-1;
+    }
+};
